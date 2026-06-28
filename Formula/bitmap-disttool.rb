3d3b3db8f7b049c79b225f9fa55aa02e0d602704
bmplatina <kkgordon.com@gmail.com>
2026-06-28 17:17:30 +0900

    bitmap-disttool v1.0.1

--- Formula/bitmap-disttool.rb
+++ Formula/bitmap-disttool.rb
@@ -1,18 +1,18 @@
 class BitmapDisttool < Formula
-  desc "Bitmap Distribution Tool foir Bitmap Developer"
-  version "1.0.0"
+  desc "Bitmap Distribution Tool for Bitmap Developer"
+  version "1.0.1"
   homepage "https://prodbybitmap.com"
   # 유니버설 바이너리 또는 단일 압축 파일 URL
-  url "https://github.com/bmplatina/desync-node/releases/download/v#{version}/bitmap_disttool_darwin_arm64",
+  url "https://github.com/bmplatina/desync-node/releases/download/v#{version}/bitmap_disttool_v#{version}_darwin_arm64",
     verified: "github.com/bmplatina/desync-node/",
     using: :nounzip
-  sha256 "41c8ab9131b0674d31e2beafe917ef3fdd8a17e1fe5988dfe8753610babd984a"
+  sha256 "b77f2c9c5389a2bb75c6bd40334643eb458616e5f2cff120e3c760312f47a049"
   license "BSD-3-Clause"
 
   def install
     # 압축 해제 시 나온 실행 바이너리를 Homebrew bin 폴더에 설치합니다.
     # bin.install은 파일을 자동으로 실행 권한(chmod +x)을 부여하여 설치해 줍니다.
-    bin.install "bitmap_disttool_darwin_arm64" => "bitmap-disttool"
+    bin.install "bitmap_disttool_v#{version}_darwin_arm64" => "bitmap-disttool"
   end
 
   # test do
